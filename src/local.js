0d100e483228b2cfc0ce0c6a8c03061694b04ddd
adnan360 <adnan360@protonmail.com>
2021-02-15 12:49:00 +0600

    Add custom json proxy from upstream
    - Edit src/local.js to customize values
    - Remove src/local.js from gitignore so that values can stay

---
+++ src/local.js
@@ -0,0 +1,10 @@
+export const urls = {
+  // The base URL of the installation
+  app: 'https://adnan360.gitlab.io/rss-box',
+  // The JSON proxy for retrieving feeds
+  proxy: 'https://adnan360.com/temp/corsproxy.php',
+  // The referrer counter
+  referrers: '',
+  // The feed to be displayed by default when opening the base URL
+  feed: 'https://lazplanet.gitlab.io/atom.xml'
+};
